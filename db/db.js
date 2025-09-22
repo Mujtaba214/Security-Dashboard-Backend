c55603bdab135bddbd27a35da5fe6759b97f974d
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-09-23 00:15:48 +0500

    Ready for deploy

--- db/db.js
+++ db/db.js
@@ -4,11 +4,8 @@ import pg from "pg";
 env.config();
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // important for Neon
 });
 
 db.connect();
