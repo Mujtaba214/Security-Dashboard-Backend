1b8fbeee77adf2785b751250e2eca632fd0a90a4
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-09-23 00:50:27 +0500

    Ready for deploy

--- server.js
+++ server.js
@@ -13,7 +13,23 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = [
+  "https://securitydashboardinfo.netlify.app", // Your deployed frontend URL
+  "http://localhost:5173", // Local development URL
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Enable cookies if needed
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api", authRoutes, apiLimiter);
 app.use("/user", userRoutes, apiLimiter);
